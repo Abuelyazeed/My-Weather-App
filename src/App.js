9d699016bee3ba57c51836eab7129105d4a7ed35
Abuelyazeed <abuelyazeed175@gmail.com>
2023-04-24 03:32:47 +0200

    fixed fetching error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const apiKey = "d403a3476ecdac8bed27848b413064e9";
+
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = async (event) => {
+    if (event.key === "Enter") {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      // console.log(response);
+      const data = await response.json();
+      //console.log(data);
+      setWeatherData(data);
+    }
+  };
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter city..."
+          aria-label="Enter city..."
+          aria-describedby="basic-addon1"
+          onChange={handleChange}
+          value={city}
+          onKeyDown={getWeather}
+        />
+      </div>
+
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>Welcome to my weather app</p>
+        </div>
+      ) : (
+        <div>
+          <p>{weatherData.name}</p>
+        </div>
+      )}
     </div>
   );
 }
